c5a81622429931bdfa355a1ccfd1c963a41b7a7a
SarasAnalytics-Daton <69815945+saras-daton@users.noreply.github.com>
2023-07-11 20:09:15 +0530

    Update SPProductAdsReport.sql

--- models/AmazonAds/SPProductAdsReport.sql
+++ models/AmazonAds/SPProductAdsReport.sql
@@ -67,7 +67,7 @@
             countryName,
             accountName,
             accountId,
-            CAST({{ dbt.dateadd(datepart="hour", interval=hr, from_date_or_timestamp="cast(reportDate as timestamp)") }} as {{ dbt.type_timestamp() }}) as reportDate,
+            reportDate,
             campaignName,
             coalesce(campaignId,'') as campaignId,
             adGroupName,
