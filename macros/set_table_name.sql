1247e8ca075d364dbdb61ef362fa4fbdcf0402ea
Saras <69815945+saras-daton@users.noreply.github.com>
2023-10-04 12:23:42 +0530

    Update set_table_name.sql

--- macros/set_table_name.sql
+++ macros/set_table_name.sql
@@ -3,7 +3,7 @@
     {% if target.type =='snowflake' %}
     select concat(table_catalog,'.',table_schema, '.',table_name) as tables,
     lower(concat(table_catalog,'.',table_schema, '.',table_name)) as tables_lowercase
-    from INFORMATION_SCHEMA.TABLES 
+    from {{ var('raw_database') }}.INFORMATION_SCHEMA.TABLES 
     where lower(table_name) like '{{variable}}'  and table_schema='{{ var("raw_schema") }}'
     {% else %}
     select concat(table_catalog,'.',table_schema, '.',table_name) as tables,
